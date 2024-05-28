87d9bb968f1c4284f861a4cda037a82713d2c204
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-05-28 13:14:38 +0200

    added configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import { fileURLToPath } from "node:url";
 import { ConfigEnv, defineConfig, loadEnv } from "vite";
 
 const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
-	const { VITE_APP_DEVELOPMENT_PORT } = loadEnv(mode, process.cwd());
-
+	const {
+		VITE_APP_API_ORIGIN_URL,
+		VITE_APP_DEVELOPMENT_PORT,
+		VITE_APP_PROXY_SERVER_URL,
+	} = loadEnv(mode, process.cwd());
 	return defineConfig({
 		build: {
 			outDir: "build",
@@ -21,6 +24,12 @@ const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
 		server: {
 			host: true,
 			port: Number(VITE_APP_DEVELOPMENT_PORT),
+			proxy: {
+				[VITE_APP_API_ORIGIN_URL]: {
+					changeOrigin: true,
+					target: VITE_APP_PROXY_SERVER_URL,
+				},
+			},
 		},
 	});
 };
