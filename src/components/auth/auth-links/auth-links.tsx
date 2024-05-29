084e2841fa3766d24189a98f5404d50eaf557fe4
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-05-29 21:01:44 +0200

    feat: split links component

--- src/components/auth/auth-links/auth-links.tsx
+++ src/components/auth/auth-links/auth-links.tsx
@@ -33,24 +33,4 @@ const AuthLinks: React.FC = () => {
 	);
 };
 
-const SignInLink: React.FC = () => {
-	const { t } = useTranslation();
-
-	return (
-		<Link to={AppRoute.SIGN_IN}>
-			<Typography color="secondary.main" component="span" variant="dmSansBold">
-				{t("SignUpComponent.doYouHaveAnAccount")}
-			</Typography>
-			<Typography
-				color="primary.main"
-				component="span"
-				ml="10px"
-				variant="playfairDisplay"
-			>
-				{t("SignUpComponent.signIn")}
-			</Typography>
-		</Link>
-	);
-};
-
-export { AuthLinks, SignInLink };
+export { AuthLinks };
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,5 +1,6 @@
-export { AuthLinks, SignInLink } from "./auth-links/auth-links.tsx";
+export { AuthLinks } from "./auth-links/auth-links.tsx";
 export { GoogleButton } from "./google-button/google-button.tsx";
 export { SignInForm } from "./sign-in-form/sign-in-form.tsx";
+export { SignInLink } from "./sign-in-link/sign-in-link.tsx";
 export { SignUpForm } from "./sign-up-form/sign-up-form.tsx";
 export { StyledFormContainer } from "./styled-form-container/styles-form-container.tsx";
