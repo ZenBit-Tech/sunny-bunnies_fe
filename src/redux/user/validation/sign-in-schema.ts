6ee0e3021eb1ab9f841c788f4a9a8458aef507f5
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-05-29 11:34:34 +0200

    feat: add user sign in types and validation schema

---
+++ src/redux/user/validation/sign-in-schema.ts
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+
+import {
+	userValidationMessage,
+	userValidationRules,
+} from "./constants/index.ts";
+
+const userSignInValidation = Yup.object().shape({
+	email: Yup.string()
+		.email(userValidationMessage.EMAIL_INVALID)
+		.required(userValidationMessage.EMAIL_REQUIRED),
+	password: Yup.string()
+		.min(
+			userValidationRules.PASSWORD_MIN_LENGTH,
+			userValidationMessage.PASSWORD_MIN_LENGTH,
+		)
+		.required(userValidationMessage.PASSWORD_REQUIRED),
+});
+
+export { userSignInValidation };
