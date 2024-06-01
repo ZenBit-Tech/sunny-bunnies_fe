2934c3d9332d75145164992b2ab7ad717ca25131
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-01 10:27:27 +0200

    feat: add images

---
+++ src/assets/images/home-page/index.ts
@@ -0,0 +1,17 @@
+import accessories from "./accessories-card.png";
+import backgroundWithBag from "./background-with-bag.png";
+import design from "./design.png";
+import goldDress from "./gold-dress.png";
+import redBag from "./red-bag.png";
+import shoes from "./shoes.png";
+
+const homePageImages = {
+	accessories,
+	backgroundWithBag,
+	design,
+	goldDress,
+	redBag,
+	shoes,
+};
+
+export { homePageImages };
