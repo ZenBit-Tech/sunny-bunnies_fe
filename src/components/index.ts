f77ab086a3c59280411fa3c02ede5b6fd35f3a5a
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-01 15:10:49 +0200

    feat: add imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
+export { Footer } from "./Footer/Footer.tsx";
 export { BaseButton } from "./base-button/index.tsx";
+export { FooterWrapper } from "./common/wrappers/footer-wrapper.tsx";
+export { HeaderWrapper } from "./common/wrappers/header-wrapper.tsx";
 export { CustomFormGroup } from "./custom-form-group/index.tsx";
 export { PrivateRoute } from "./private-route/index.tsx";
 export { PublicRoute } from "./public-route/index.tsx";
