b43b9fcd57295a38f2fc7845c05b2161b70e3d97
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-01 10:31:16 +0200

    feat: add imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { Footer } from "./Footer/Footer.tsx";
+export { Header } from "./Header/Header.tsx";
 export { BaseButton } from "./base-button/index.tsx";
 export { CustomFormGroup } from "./custom-form-group/index.tsx";
 export { ExampleForm } from "./example-form/example-form.tsx";
