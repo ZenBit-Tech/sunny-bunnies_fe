91e6759d4a0da7eab2c44f4e2aa463fabd9b8b89
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-03 01:51:38 +0200

    feat: delete example files

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { Footer } from "./Footer/Footer.tsx";
 export { Header } from "./Header/Header.tsx";
 export { BaseButton } from "./base-button/index.tsx";
 export { CustomFormGroup } from "./custom-form-group/index.tsx";
-export { ExampleForm } from "./example-form/example-form.tsx";
 export { PrivateRoute } from "./private-route/index.tsx";
 export { PublicRoute } from "./public-route/index.tsx";
 export { RouterProvider } from "./router-provider/index.tsx";
