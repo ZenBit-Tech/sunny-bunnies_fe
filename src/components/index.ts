81fa3fbfba378138069aa1f7a5c08e8951393f14
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-05 20:39:02 +0200

    feat: add loader

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { FooterWrapper } from "./common/wrappers/footer-wrapper.tsx";
 export { HeaderWrapper } from "./common/wrappers/header-wrapper.tsx";
 export { CustomFormGroup } from "./custom-form-group/index.tsx";
 export { FilterButton } from "./filter-button/index.tsx";
+export { Loader } from "./loader/index.tsx";
 export { Newsletter } from "./newsletter/index.tsx";
 export { PrivateRoute } from "./private-route/index.tsx";
 export { PublicRoute } from "./public-route/index.tsx";
