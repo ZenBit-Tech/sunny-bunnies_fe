e1bf4dc334483d433e284e1a52e729bed73a7b19
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-07 14:52:46 +0200

    feat: add not found page

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ import {
 } from "./components/index.ts";
 import { AppRoute } from "./libs/constants/index.ts";
 import "./libs/locales/i18n.ts";
-import { Auth, Home } from "./pages/index.ts";
+import { Auth, Home, NotFound } from "./pages/index.ts";
 import { PrivacyPolicy } from "./pages/privacy-policy/privacy-policy-page.tsx";
 import { TermsConditions } from "./pages/terms-conditions/terms-and-conditions.tsx";
 import { persistor, store } from "./redux/store.ts";
@@ -82,6 +82,10 @@ createRoot(document.getElementById("root")!).render(
 									],
 									element: <App />,
 									path: AppRoute.ROOT,
+								},
+								{
+									element: <NotFound />,
+									path: AppRoute.ANY,
 								},
 							]}
 						/>
---
+++ src/pages/not-found/index.tsx
@@ -0,0 +1,68 @@
+import { Link, Typography } from "@mui/material";
+import React, { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+import { BaseButton } from "~/components/index.ts";
+import {
+	AppRoute,
+	colors,
+	fontSizes,
+	fontWeight,
+} from "~/libs/constants/index.ts";
+
+import {
+	StyledNotFoundContainer,
+	StyledNotFoundContentContainer,
+	StyledNotFoundContentOptions,
+	StyledTypography,
+} from "./styles.ts";
+
+const NotFound: React.FC = () => {
+	const navigate = useNavigate();
+	const { t } = useTranslation();
+
+	const handleNavigate = useCallback(() => {
+		navigate(AppRoute.HOME);
+	}, [navigate]);
+
+	return (
+		<StyledNotFoundContainer>
+			<StyledNotFoundContentContainer>
+				<Typography mb={2} variant="playfairDisplayTitle">
+					{t("NotFoundPage.pageNotFound")}
+				</Typography>
+				<StyledTypography paragraph variant="dmSans">
+					{t("NotFoundPage.weAreSorry")}
+				</StyledTypography>
+				<StyledNotFoundContentOptions>
+					<Typography fontSize={fontSizes.large} paragraph variant="dmSansBold">
+						{t("NotFoundPage.hereAreFewOptions")}
+					</Typography>
+					<Typography paragraph variant="dmSans">
+						{t("NotFoundPage.doubleCheckTheUrl")}
+					</Typography>
+					<Typography paragraph variant="dmSans">
+						{t("NotFoundPage.goBackToThePreviousPage")}
+					</Typography>
+				</StyledNotFoundContentOptions>
+				<StyledTypography paragraph variant="dmSans">
+					{t("NotFoundPage.ifYouBelieve")}
+					<Link
+						color={colors.black}
+						fontWeight={fontWeight.bold}
+						href={t("NotFoundPage.emailLink")}
+					>
+						{t("NotFoundPage.emailLinkExample")}
+					</Link>
+					{t("NotFoundPage.weWillHelpYou")}
+				</StyledTypography>
+				<BaseButton onClick={handleNavigate} variant="primary_black_bold">
+					{t("NotFoundPage.goToHome")}
+				</BaseButton>
+			</StyledNotFoundContentContainer>
+		</StyledNotFoundContainer>
+	);
+};
+
+export { NotFound };
