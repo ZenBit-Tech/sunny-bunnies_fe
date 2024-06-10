a41f04556f73ed0982795f9b171d9c83fce68123
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-10 13:09:39 +0200

    fix: add link to header buttons

--- src/libs/constants/index.ts
+++ src/libs/constants/index.ts
@@ -2,6 +2,7 @@ export { AppRoute } from "./app-route.ts";
 export { colors } from "./colors.ts";
 export { fontFamily, fontSizes, fontWeight } from "./fonts.ts";
 export { gender } from "./gender.ts";
+export { headerLogOutLinks, headerLoginLinks } from "./header-links.ts";
 export { httpMethods } from "./http-methods.ts";
 export { httpStatusCode } from "./http-status-code.ts";
 export { productStatus } from "./product-status.ts";
