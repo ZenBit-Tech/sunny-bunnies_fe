45a03a1c6a86e21f86148eb1adae796244c5ba27
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-14 18:36:56 +0200

    feat: add images

---
+++ src/assets/images/product/index.ts
@@ -0,0 +1,11 @@
+import ShopIcon from "./bag-check.svg?react";
+import MessengerIcon from "./chat-dots.svg?react";
+import WishListIcon from "./wish-list-heart.svg?react";
+
+const productIcons = {
+	MessengerIcon,
+	ShopIcon,
+	WishListIcon,
+};
+
+export { productIcons };
