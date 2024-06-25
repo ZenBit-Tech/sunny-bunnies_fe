ee12774e9eca293aeb10b44b95168c79a546e42e
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-25 14:28:46 +0200

    fix: align carousel items on home page

--- src/pages/home/components/category-carousel/styles.ts
+++ src/pages/home/components/category-carousel/styles.ts
@@ -4,7 +4,7 @@ const StyledCategoryCarouselContainer = styled(Box)`
 	align-items: flex-start;
 	display: flex;
 	flex-direction: column;
-	justify-content: flex-start;
+	justify-content: space-between;
 	padding: 15px 52px;
 	width: 100%;
 `;
@@ -13,16 +13,14 @@ const StyledCategoriesContainer = styled(Box)`
 	align-items: center;
 	display: flex;
 	gap: 20px;
-	justify-content: center;
+	justify-content: space-between;
 	width: 100%;
 `;
 
 const StyledCategoriesItems = styled(Box)`
 	display: flex;
-	gap: 30px;
+	justify-content: space-between;
 	flex-wrap: nowrap;
-	overflow-x: auto;
-	justify-content: space-evenly;
 	width: 100%;
 `;
 
