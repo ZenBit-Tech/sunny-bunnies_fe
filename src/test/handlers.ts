ec49f158e382f68447ccc402643bf4365875efea
Vfivew <hitori555111555@yahoo.com>
2024-07-03 01:55:29 +0300

    Revert "fix: add test case for api"
    This reverts commit 463dff6a610f5b08526c5d8b697cb40b3f8b596f.

--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -1,6 +1,5 @@
 import { HttpResponse, http } from "msw";
 
-import { authApiPath } from "~/redux/auth/constants.ts";
 import { filtersApiPath } from "~/redux/filters/constants.ts";
 
 import { allFilters } from "./mocks/all-filters.ts";
@@ -10,11 +9,6 @@ const apiUrl = import.meta.env.VITE_BASE_URL;
 const handlers = [
 	http.get(`${apiUrl}${filtersApiPath.ROOT}`, () => {
 		return HttpResponse.json(allFilters);
-	}),
-	http.post(`${apiUrl}${authApiPath.RESTORE_PASSWORD}`, () => {
-		return HttpResponse.json({
-			status: 200,
-		});
 	}),
 ];
 
