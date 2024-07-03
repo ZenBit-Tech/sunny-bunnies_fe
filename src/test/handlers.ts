26cafabf3a4346ea9b44e453a3578d110751afaf
Vfivew <hitori555111555@yahoo.com>
2024-07-03 13:48:44 +0300

    fix: add api test

--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -1,14 +1,31 @@
 import { HttpResponse, http } from "msw";
 
+import { adminApiPath } from "~/redux/admin/constants.ts";
 import { filtersApiPath } from "~/redux/filters/constants.ts";
 
-import { allFilters } from "./mocks/all-filters.ts";
+import { allFilters, user, users } from "./mocks/index.ts";
 
 const apiUrl = import.meta.env.VITE_BASE_URL;
 
 const handlers = [
 	http.get(`${apiUrl}${filtersApiPath.ROOT}`, () => {
 		return HttpResponse.json(allFilters);
+	}),
+	http.delete(`${apiUrl}${adminApiPath.DELETE_USER}/:id`, () => {
+		return HttpResponse.json({
+			status: 200,
+		});
+	}),
+	http.patch(`${apiUrl}${adminApiPath.UPDATE_USER_STATUS}/:id`, () => {
+		return HttpResponse.json({
+			status: 200,
+		});
+	}),
+	http.get(`${apiUrl}${adminApiPath.GET_USER}/:id`, () => {
+		return HttpResponse.json(user);
+	}),
+	http.get(`${apiUrl}${adminApiPath.GET_BY_OPTIONS}`, () => {
+		return HttpResponse.json(users);
 	}),
 ];
 
