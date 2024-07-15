b2dfec180a421db0612af4ae5dddc1fc5f00dd56
Askabek Djanbolotov <djanbolotov03@gmail.com>
2024-07-15 23:19:43 +0600

    Changed styles

--- src/pages/profile-board/components/buttons.tsx
+++ src/pages/profile-board/components/buttons.tsx
@@ -2,8 +2,7 @@ import React from "react";
 
 import { t } from "i18next";
 
-import { BaseButton } from "~/components/index.ts";
-import theme from "~/theme.ts";
+import { StyledButton } from "./styles.ts";
 
 type FormButtonsProps = {
 	handleNext?: () => void;
@@ -19,38 +18,20 @@ const FormButtons: React.FC<FormButtonsProps> = ({
 	return (
 		<React.Fragment>
 			{!isStart && (
-				<BaseButton
-					sx={{
-						border: "1px solid",
-						borderRadius: "8px",
-						gap: "8px",
-						height: "34px",
-						padding: "8px 24px",
-						textTransform: "none",
-						width: "78px",
-					}}
+				<StyledButton
 					to={redirectTo}
 					variant="outlined"
 				>
 					{t("Form.prev")}
-				</BaseButton>
+				</StyledButton>
 			)}
-			<BaseButton
+			<StyledButton
 				onClick={handleNext}
-				sx={{
-					border: `1px solid ${theme.palette.transparent}`,
-					borderRadius: "8px",
-					gap: "8px",
-					height: "34px",
-					padding: "8px 24px",
-					textTransform: "none",
-					width: "78px",
-				}}
 				type={handleNext ? "button" : "submit"}
 				variant="contained"
 			>
 				{t("Form.next")}
-			</BaseButton>
+			</StyledButton>
 		</React.Fragment>
 	);
 };
--- src/pages/profile-board/components/styles.ts
+++ src/pages/profile-board/components/styles.ts
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Box } from "@mui/material";
+import {BaseButton} from "~/components";
 
 const StyledFormContainer = styled(Box)`
 	align-items: flex-start;
@@ -22,4 +23,14 @@ const VisuallyHiddenInput = styled("input")({
 	width: "1px",
 });
 
-export { StyledFormContainer, VisuallyHiddenInput };
+const StyledButton = styled(BaseButton) ({
+	border: "1px solid",
+	borderRadius: "8px",
+	gap: "8px",
+	height: "34px",
+	padding: "8px 24px",
+	textTransform: "none",
+	width: "78px",
+});
+
+export { StyledFormContainer, VisuallyHiddenInput, StyledButton };
